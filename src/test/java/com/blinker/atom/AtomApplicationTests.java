caa3492ae3e34c4d64d97ea66d83aa64b8e82edf
cm-JongwonKang <jongwon_kang@tmax.co.kr>
2025-01-02 15:56:07 +0900

    Initial commit

---
+++ src/test/java/com/blinker/atom/AtomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blinker.atom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
