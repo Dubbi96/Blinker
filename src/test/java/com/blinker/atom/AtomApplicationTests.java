312912227f0d89b303dc9ea242844bc95e498087
cm-JongwonKang <jongwon_kang@tmax.co.kr>
2025-01-06 02:12:20 +0900

    chore : CI/CD 절차 수정

--- src/test/java/com/blinker/atom/AtomApplicationTests.java
+++ src/test/java/com/blinker/atom/AtomApplicationTests.java
@@ -1,10 +1,22 @@
 package com.blinker.atom;
 
+import com.blinker.atom.service.AuthService;
+import com.blinker.atom.service.CustomUserDetailsService;
+import com.blinker.atom.service.SensorService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
+@RequiredArgsConstructor
 class AtomApplicationTests {
+
+	@Mock
+	private AuthService	authService;
+	@Mock
+	private CustomUserDetailsService customUserDetailsService;
+	@Mock
+	private SensorService sensorService;
 
 	@Test
 	void contextLoads() {
